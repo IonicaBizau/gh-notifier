45e44968908eb88ed860c10622d653ef6f5873fe
Ionică Bizău <bizauionica@gmail.com>
2016-01-12 11:21:51 +0200

    :fire: nuke token
    Yes, I deleted it from my personal tokens. :joy:

--- example/index.js
+++ example/index.js
@@ -3,7 +3,7 @@
 const notify = require("../lib");
 
 // Check every 3 seconds
-notify("b80688e1decdca2cd0ffc8b665e4aa14fc5ba8c4", 3000, (err, data) => {
+notify("b80688e1...fc5ba8c4", 3000, (err, data) => {
     console.log(err || `${data.title[0]._} ${data.published[0]}`);
 });
 // GhitaB forked IonicaBizau/Resources to GhitaB/Resources 2016-01-12T09:03:22Z
